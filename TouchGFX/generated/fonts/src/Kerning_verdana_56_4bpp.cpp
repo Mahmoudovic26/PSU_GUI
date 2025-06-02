e49449d50cee5ed3814b71d9c324dc5e6fb97d59
Mahmoudovic26 <hodaoda83@gmail.com>
2025-06-02 19:42:36 +0300

    basic PSU gui has been implemented

---
+++ TouchGFX/generated/fonts/src/Kerning_verdana_56_4bpp.cpp
@@ -0,0 +1,8 @@
+#include <touchgfx/Font.hpp>
+
+FONT_KERNING_LOCATION_FLASH_PRAGMA
+KEEP extern const touchgfx::KerningNode kerning_verdana_56_4bpp[] FONT_KERNING_LOCATION_FLASH_ATTRIBUTE = {
+    { 0x0056, -8 },  // (First char = [0x0056, V], Second char = [0x002E, .], Kerning dist = -8)
+    { 0x0056, -2 },  // (First char = [0x0056, V], Second char = [0x0041, A], Kerning dist = -2)
+    { 0x0041, -2 },  // (First char = [0x0041, A], Second char = [0x0056, V], Kerning dist = -2)
+};
